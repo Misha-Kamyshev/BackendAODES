fa3a72d3caadc3c18c245cc05a121936a95580be
Misha-Kamyshev <kam628326@gmail.com>
2026-05-24 18:38:28 +1000

    Исправлено название файла с политикой

--- app/routers/static_files.py
+++ app/routers/static_files.py
@@ -11,7 +11,7 @@ router = APIRouter(prefix="/static_files", tags=["static-files"])
 @router.get("/policy", response_model=PolicyResponse)
 def get_privacy_policy():
     try:
-        with open("data/policy.json", "r", encoding="utf-8") as file:
+        with open("data/privacy.json", "r", encoding="utf-8") as file:
             return json.load(file)
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="Privacy policy file not found")
