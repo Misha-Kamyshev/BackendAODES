cee1d36fc78db5519aa0116c7dcff87828b32414
Misha-Kamyshev <kam628326@gmail.com>
2026-06-13 20:56:34 +1000

    Исправлена политика и добавлен эндпоинт согласия

--- app/routers/static_files.py
+++ app/routers/static_files.py
@@ -8,10 +8,19 @@ from app.schemas.static_files import PolicyResponse
 router = APIRouter(prefix="/static_files", tags=["static-files"])
 
 
-@router.get("/policy", response_model=PolicyResponse)
-def get_privacy_policy():
+@router.get("/privacy", response_model=PolicyResponse)
+def get_privacy():
     try:
         with open("data/privacy.json", "r", encoding="utf-8") as file:
             return json.load(file)
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="Privacy policy file not found")
+
+
+@router.get("/agreement", response_model=PolicyResponse)
+def get_agreement():
+    try:
+        with open("data/agreement.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Agreement file not found")
